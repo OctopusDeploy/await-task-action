edcd8584531fc983f8a13b7a7eef0001eb0216db
Grace Rehn <grace.rehn@octopus.com>
2025-07-17 10:34:00 +1000

    chore: Fix unit test

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -8,5 +8,6 @@ process.env = Object.assign(process.env, {
   INPUT_API_KEY: process.env['OCTOPUS_API_KEY'],
   INPUT_SPACE: 'Default',
   INPUT_SERVER_TASK_ID: 'ServerTasks-123',
-  INPUT_HIDE_PROGRESS: 'false'
+  INPUT_HIDE_PROGRESS: 'false',
+  INPUT_CANCEL_ON_TIMEOUT: 'false'
 })
